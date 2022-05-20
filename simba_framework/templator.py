9d23eda21b7ce81d05d1a3e4696aacdc4584dc9b
Andrey <dorst.andreas@gmail.com>
2022-05-20 15:12:26 +0300

    feature

--- simba_framework/templator.py
+++ simba_framework/templator.py
@@ -1,5 +1,7 @@
 from os.path import join
 from jinja2 import Template
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
@@ -9,10 +11,19 @@ def render(template_name, folder='templates', **kwargs):
     :param kwargs: parameters
     :return:
     """
-    file_path = join(folder, template_name)
-    # Open template by name
-    with open(file_path, encoding='utf-8') as f:
-        # Read
-        template = Template(f.read())
-    # Rendering template with parameters
+
+    # file_path = join(folder, template_name)
+    # # Open template by name
+    # with open(file_path, encoding='utf-8') as f:
+    #     # Read
+    #     template = Template(f.read())
+    # # Rendering template with parameters
+    # return template.render(**kwargs)
+
+    # create environment object
+    env = Environment()
+    # Specify the folder to search for templates
+    env.loader = FileSystemLoader(folder)
+    # Find template in environment
+    template = env.get_template(template_name)
     return template.render(**kwargs)
